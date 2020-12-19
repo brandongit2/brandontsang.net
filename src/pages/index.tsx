4d74b8c8964147a8a633b3a07be90ae0e4dabcc9
Brandon Tsang <brandononline2@gmail.com>
2020-12-19 00:53:47 -0500

    Editable text in middle of screen.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,34 @@
+import Head from 'next/head';
+import {useState} from 'react';
+
+import styles from './index.module.scss';
+import ShadedLetter from '../components/ShadedLetter';
+
 export default function Home() {
-    return <p>Hi!</p>;
+    const [text, setText] = useState('Click to edit me!');
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://use.typekit.net/lni7lly.css"
+                />
+            </Head>
+            <div style={{position: 'relative'}}>
+                <input
+                    className={`${styles.title} ${styles.input}`}
+                    value={text}
+                    onInput={(evt: React.ChangeEvent<HTMLInputElement>) => {
+                        setText(evt.target.value);
+                    }}
+                />
+                <h1 className={`${styles.title} ${styles.display}`}>
+                    {text.split('').map((letter) => (
+                        <ShadedLetter>{letter}</ShadedLetter>
+                    ))}
+                </h1>
+            </div>
+        </div>
+    );
 }
