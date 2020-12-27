1675a196bea57b9bfd707affe3e41b0caaa7c2d6
Brandon Tsang <brandononline2@gmail.com>
2020-12-27 16:49:12 -0500

    Added "About" section.

--- src/components/homepage/Title.tsx
+++ src/components/homepage/Title.tsx
@@ -4,6 +4,7 @@ import styles from './Title.module.scss';
 import {ThemeColorContext} from '../../contexts/ThemeColors';
 import {useShadow} from '../../hooks/useShadow';
 import Color from 'color';
+import ColoredImg from '../ColoredImg';
 
 export default function Slide1() {
     const {back, fore} = useContext(ThemeColorContext);
@@ -39,13 +40,12 @@ export default function Slide1() {
             </h2>
             <p className={styles.action} style={{boxShadow: shadow}}>
                 Scroll to read more
-                <svg viewBox="0 0 476.213 476.213" className={styles.arrow}>
-                    <polygon
-                        points="345.606,107.5 324.394,128.713 418.787,223.107 0,223.107 0,253.107 418.787,253.107 324.394,347.5 
-	345.606,368.713 476.213,238.106"
-                        fill="var(--foreground-color)"
-                    />
-                </svg>
+                <ColoredImg
+                    src="right-arrow.svg"
+                    color={fore.string()}
+                    height="1em"
+                    className={styles.arrow}
+                />
             </p>
         </div>
     );
