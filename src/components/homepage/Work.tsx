21f8981ee7c9917d1a9fe85a58407b8a06e16ad1
Brandon Tsang <brandononline2@gmail.com>
2021-01-23 00:05:50 -0500

    CSS cleanup.

--- src/components/homepage/Work.tsx
+++ src/components/homepage/Work.tsx
@@ -7,11 +7,17 @@ const Work = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div ref={ref} className={styles.container}>
             <h1 className={styles.title}>My Work</h1>
-            <div className={workStyle.container}>
-                <div className={workStyle.image} style={{backgroundImage: ''}}>
-                    <h1 className={workStyle.title}>brandontsang.net</h1>
+            <div className={styles.content}>
+                <div className={workStyle.container}>
+                    <div className={workStyle.titleContainer}>
+                        <h1 className={workStyle.title}>brandontsang.net</h1>
+                    </div>
+                    <div
+                        className={workStyle.image}
+                        style={{backgroundImage: ''}}
+                    />
+                    <p className={workStyle.description}></p>
                 </div>
-                <p className={workStyle.description}></p>
             </div>
         </div>
     );
