64e887ddb7bc9c53d1e31d403202b9d99b8b35a5
Brandon Tsang <brandononline2@gmail.com>
2021-01-23 20:11:19 -0500

    My Work: perfomance improvements.

--- src/misc/util.ts
+++ src/misc/util.ts
@@ -1,3 +1,9 @@
 export function clamp(num: number, min: number, max: number) {
     return num <= min ? min : num >= max ? max : num;
 }
+
+export function remToPixels(rem: number) {
+    return (
+        rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
+    );
+}
