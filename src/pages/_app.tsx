0e6aca72a1f765cf8f2073a665a50f82085300ef
Brandon Tsang <brandononline2@gmail.com>
2021-02-06 15:03:35 -0500

    Removed Firebase.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,9 @@
 import {AppProps} from 'next/app';
-import {FirebaseAppProvider} from 'reactfire';
 
 import './_app.scss';
 
-const firebaseConfig = {
-    apiKey: 'AIzaSyCxCqQ3tMjELwyXyVUuxG74gorGC2CRudM',
-    authDomain: 'brandontsang-net.firebaseapp.com',
-    projectId: 'brandontsang-net',
-    storageBucket: 'brandontsang-net.appspot.com',
-    messagingSenderId: '903224799056',
-    appId: '1:903224799056:web:cb76f775e575ab365f0655',
-    measurementId: 'G-G39ZLTRJXV'
-};
-
 function MyApp({Component, pageProps}: AppProps) {
-    return (
-        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-            <Component {...pageProps} />
-        </FirebaseAppProvider>
-    );
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
