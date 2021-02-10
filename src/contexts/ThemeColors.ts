3bbbea2271eec6a1936036eeb180ac0263c0431a
Brandon Tsang <brandononline2@gmail.com>
2021-02-09 19:05:31 -0500

    Switched to styled-components; changed code style.

--- src/contexts/ThemeColors.ts
+++ src/contexts/ThemeColors.ts
@@ -1,7 +1,7 @@
-import Color from 'color';
-import {createContext} from 'react';
+import Color from "color";
+import {createContext} from "react";
 
 export const ThemeColorContext = createContext({
     back: Color(),
-    fore: Color()
+    fore: Color(),
 });
