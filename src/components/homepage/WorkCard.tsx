8a8c93151117a25c7a29c60eda4ae5a3e3672e5a
Brandon Tsang <brandononline2@gmail.com>
2021-02-10 21:50:26 -0500

    Fixed responsive layout bugs.

--- src/components/homepage/WorkCard.tsx
+++ src/components/homepage/WorkCard.tsx
@@ -39,10 +39,16 @@ const Info = styled.div`
     left: 0px;
     bottom: 0px;
     z-index: 2;
-    width: 100%;
-    height: 5rem;
-    padding: 0.5rem 1.5rem;
-    margin-bottom: 1rem;
+    width: calc(100% - 3rem);
+    height: 4em;
+    padding: 0.5em 1.5em;
+    margin-bottom: 1em;
+    display: flex;
+    align-items: stretch;
+    justify-content: space-between;
+`;
+
+const InfoLeft = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -53,7 +59,7 @@ const Shadow = styled.div`
     left: 0px;
     bottom: 0px;
     width: 100%;
-    height: calc(7rem - 160px);
+    height: calc(6.5em - 160px);
 `;
 
 const Border = styled.div`
@@ -61,10 +67,20 @@ const Border = styled.div`
     z-index: -1;
     border: 2px solid var(--foreground-color);
     width: 100%;
-    height: 7rem;
+    height: 6.5em;
     left: 0px;
     bottom: 0px;
     box-sizing: border-box;
+`;
+
+const TechContainer = styled.div`
+    align-self: flex-end;
+    display: flex;
+`;
+
+const Tech = styled.img`
+    height: 1.3em;
+    margin: 0px 0.3em;
 `;
 
 interface SitePreviewProps {
@@ -91,16 +107,46 @@ interface WorkCardProps {
     url: string;
     title: string;
     description: string;
+    techStack: Array<
+        "typescript" | "react" | "nextjs" | "redux" | "firebase" | "godot"
+    >;
 }
 
-export default function WorkCard({url, title, description}: WorkCardProps) {
+export default function WorkCard({
+    url,
+    title,
+    description,
+    techStack,
+}: WorkCardProps) {
     return (
         <Container>
             <Border />
             <SitePreview src={url} />
             <Info>
-                <h1>{title}</h1>
-                <p>{description}</p>
+                <InfoLeft>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </InfoLeft>
+                <TechContainer>
+                    {techStack.includes("typescript") && (
+                        <Tech src="typescript-logo.svg" />
+                    )}
+                    {techStack.includes("react") && (
+                        <Tech src="react-logo.svg" />
+                    )}
+                    {techStack.includes("redux") && (
+                        <Tech src="redux-logo.svg" />
+                    )}
+                    {techStack.includes("nextjs") && (
+                        <Tech src="nextjs-logo.svg" />
+                    )}
+                    {techStack.includes("firebase") && (
+                        <Tech src="firebase-logo.svg" />
+                    )}
+                    {techStack.includes("godot") && (
+                        <Tech src="godot-logo.svg" />
+                    )}
+                </TechContainer>
             </Info>
         </Container>
     );
