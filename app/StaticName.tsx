72937bd06e6b0525855373b9abf6084127660d12
Brandon Tsang <brandononline2@gmail.com>
2022-11-08 18:51:14 -0500

    fix types

--- app/StaticName.tsx
+++ app/StaticName.tsx
@@ -1,37 +1,35 @@
+import {useTexture} from "@react-three/drei"
+import {useFrame, useThree} from "@react-three/fiber"
+import {useEffect, useRef, useState} from "react"
+
 import type {ReactElement} from "react"
+import StaticMaterial from "./StaticMaterial"
+import {ShaderMaterial} from "three"
+
+const nameMapAspect = 2132 / 1322 // Dimensions of name-map.png
 
 const StaticName = (): ReactElement | null => {
-	// const map = useTexture(`/name-map.png`)
-	// const map = useLoader(TextureLoader, `/name-map.png`)
+	const nameMap = useTexture(`/name-map.png`)
+
+	// Fit the name box in the canvas
+	const [nameBoxHeight, setNameBoxHeight] = useState(0)
+	const viewport = useThree((state) => state.viewport)
+	useEffect(() => {
+		setNameBoxHeight(viewport.aspect > nameMapAspect ? viewport.height : viewport.width / nameMapAspect)
+	}, [viewport.aspect, viewport.height, viewport.width])
+
+	// Update time uniform
+	const staticRef = useRef<ShaderMaterial | null>(null)
+	useFrame((state, delta) => {
+		if (!staticRef.current) return
+		staticRef.current.uniforms.time.value += delta
+	})
 
 	return (
-		<>
-			{/* <mesh>
-				<planeGeometry />
-				<meshBasicMaterial map={map} />
-			</mesh> */}
-
-			<mesh scale={2}>
-				<boxGeometry />
-				<meshBasicMaterial color={0x00ff00} />
-			</mesh>
-			{/* <mesh position={[-100, -100, 0]} scale={30}>
-				<boxGeometry />
-				<meshBasicMaterial color={0x00ff00} />
-			</mesh>
-			<mesh position={[100, -100, 0]} scale={30}>
-				<boxGeometry />
-				<meshBasicMaterial color={0x00ff00} />
-			</mesh>
-			<mesh position={[-100, 100, 0]} scale={30}>
-				<boxGeometry />
-				<meshBasicMaterial color={0x00ff00} />
-			</mesh>
-			<mesh position={[100, 100, 0]} scale={30}>
-				<boxGeometry />
-				<meshBasicMaterial color={0x00ff00} />
-			</mesh> */}
-		</>
+		<mesh scale={[nameBoxHeight * nameMapAspect, nameBoxHeight, 1]}>
+			<planeGeometry />
+			<staticMaterial key={StaticMaterial.key} nameMap={nameMap} ref={staticRef} />
+		</mesh>
 	)
 }
 
